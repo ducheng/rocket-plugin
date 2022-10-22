0d6639dc7d94d4b613ed1a9de5171827e26d3921
USER-20200810GJ\Administrator <13275768320@163.com>
2022-10-22 23:05:14 +0800

    代码大规模的修改

--- src/main/java/com/ducheng/rocket/config/DelayBasedRocketMQAutoConfiguration.java
+++ src/main/java/com/ducheng/rocket/config/DelayBasedRocketMQAutoConfiguration.java
@@ -1,51 +1,23 @@
 package com.ducheng.rocket.config;
 
-import com.ducheng.rocket.annotation.DelayBasedRocketMQ;
 import com.ducheng.rocket.delay.DelayConsumerContainerRegistry;
-import com.ducheng.rocket.delay.DelayMethodInterceptor;
 import org.apache.rocketmq.spring.autoconfigure.RocketMQAutoConfiguration;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.aop.PointcutAdvisor;
-import org.springframework.aop.support.DefaultPointcutAdvisor;
-import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.DefaultParameterNameDiscoverer;
-import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.core.env.Environment;
 
 
 @Configuration
 @AutoConfigureAfter(RocketMQAutoConfiguration.class)
-@ConditionalOnBean(RocketMQTemplate.class)
 public class DelayBasedRocketMQAutoConfiguration {
-
-    private final ParameterNameDiscoverer parameterNameDiscoverer = new DefaultParameterNameDiscoverer();
 
     @Autowired
     private Environment environment;
 
-    @Autowired
-    private RocketMQTemplate rocketMQTemplate;
-
-    @Bean
-    public DelayMethodInterceptor delayInterceptor(){
-        return new DelayMethodInterceptor(this.environment, this.rocketMQTemplate, parameterNameDiscoverer);
-    }
-
     @Bean
     public DelayConsumerContainerRegistry delayConsumerContainerRegistry(){
-        return new DelayConsumerContainerRegistry(this.environment);
-    }
-
-
-
-    @Bean
-    public PointcutAdvisor delayPointcutAdvisor(@Autowired DelayMethodInterceptor delayMethodInterceptor){
-        return new DefaultPointcutAdvisor(new AnnotationMatchingPointcut(null, DelayBasedRocketMQ.class),
-                delayMethodInterceptor);
+        return new DelayConsumerContainerRegistry(environment);
     }
 }
--- src/main/java/com/ducheng/rocket/delay/DelayConsumerContainerRegistry.java
+++ src/main/java/com/ducheng/rocket/delay/DelayConsumerContainerRegistry.java
@@ -1,27 +1,34 @@
 package com.ducheng.rocket.delay;
 
-import com.ducheng.rocket.annotation.DelayBasedRocketMQ;
+import com.ducheng.rocket.annotation.DelayRocketMQ;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.reflect.MethodUtils;
 import org.springframework.aop.framework.AopProxyUtils;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.core.env.Environment;
 
 import java.lang.reflect.Method;
 import java.util.List;
 
-/**
- * Created by taoli on 2022/9/4.
- * gitee : https://gitee.com/litao851025/lego
- * 编程就像玩 Lego
- */
-public class DelayConsumerContainerRegistry
-    extends AbstractConsumerContainerRegistry {
+@Slf4j
+public class DelayConsumerContainerRegistry implements BeanPostProcessor {
+
+    private Environment environment;
+
+    public Environment getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
 
     public DelayConsumerContainerRegistry(Environment environment) {
-        super(environment);
+        this.environment = environment;
     }
 
     @SneakyThrows
@@ -29,26 +36,19 @@ public class DelayConsumerContainerRegistry
     public Object postProcessAfterInitialization(Object proxy, String beanName) throws BeansException {
         // 1. 获取 @DelayBasedRocketMQ 注解方法
         Class targetCls = AopUtils.getTargetClass(proxy);
-        List<Method> methodsListWithAnnotation = MethodUtils.getMethodsListWithAnnotation(targetCls, DelayBasedRocketMQ.class);
-
+        List<Method> methodsListWithAnnotation = MethodUtils.getMethodsListWithAnnotation(targetCls, DelayRocketMQ.class);
         // 2. 为每个 @DelayBasedRocketMQ 注解方法 注册 RocketMQConsumerContainer
         for(Method method : methodsListWithAnnotation){
-            DelayBasedRocketMQ annotation = AnnotatedElementUtils.findMergedAnnotation(method, DelayBasedRocketMQ.class);
-            String consumerProfile = annotation.consumerProfile();
-            if (!isActiveProfile(consumerProfile)){
-                continue;
-            }
-
+            DelayRocketMQ annotation = AnnotatedElementUtils.findMergedAnnotation(method, DelayRocketMQ.class);
+            //获取真实的代理对象
             Object bean = AopProxyUtils.getSingletonTarget(proxy);
             DelayConsumerContainer delayConsumerContainer =
-                    new DelayConsumerContainer(this.getEnvironment(),
+                    new DelayConsumerContainer(
                             annotation,
                             bean,
-                            method);
-            delayConsumerContainer.afterPropertiesSet();
-
-            this.getConsumerContainers().add(delayConsumerContainer);
+                            method, environment);
         }
         return proxy;
     }
+
 }
